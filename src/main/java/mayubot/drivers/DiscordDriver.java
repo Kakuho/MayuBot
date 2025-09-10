3d323e655d6fa9f8629b0695e1ecc15d08a88571
ka <kaku.h9@gmail.com>
2025-09-10 16:35:17 +0100

    driver: refactored discord driver and mayulistener

--- src/main/java/mayubot/drivers/DiscordDriver.java
+++ src/main/java/mayubot/drivers/DiscordDriver.java
@@ -2,20 +2,49 @@ package mayubot.drivers;
 
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.requests.GatewayIntent;
-import net.dv8tion.jda.api.hooks.ListenerAdapter;
-import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
 import java.util.EnumSet;
 
-public class DiscordDriver{
+public class DiscordDriver extends Driver{
+  private MayuListener mayuListener;
   public DiscordDriver(){
-  var token = System.getenv("TOKEN");
+    super();
+    this.mayuListener = new MayuListener();
+    this.mayuListener.SetDriver(this);
+  }
+
+  public DiscordDriver(Mode mode){
+    super(mode);
+    this.mayuListener = new MayuListener();
+    this.mayuListener.SetDriver(this);
+  }
+
+  // main event loop handlers
+
+  @Override
+  protected void HandleFailure(){
+    System.out.println("You miscounted! Count is resetting back to 1");
+  }
+
+  @Override
+  protected void HandleSuccess(){
+    System.out.println("Success!");
+  }
+
+  @Override
+  public void Run(){
+    var token = System.getenv("TOKEN");
     if(token == null){
       System.out.println("invalid token, exiting");
     }
     else{
-      JDABuilder.createLight(token, EnumSet.of(GatewayIntent.GUILD_MESSAGES, GatewayIntent.MESSAGE_CONTENT))
-                .addEventListeners(new MayuListener(MayuListener.Mode.Standard))
+      var intents = EnumSet.of(
+        GatewayIntent.GUILD_MESSAGES, 
+        GatewayIntent.MESSAGE_CONTENT
+      );
+
+      JDABuilder.createLight(token, intents)
+                .addEventListeners(this.mayuListener)
                 .build();
     }
   }
--- src/main/java/mayubot/drivers/MayuListener.java
+++ src/main/java/mayubot/drivers/MayuListener.java
@@ -1,104 +1,14 @@
 package mayubot.drivers;
 
-import mayubot.Evaluator;
-import mayubot.ast.EvaluationResult;
-
-import net.dv8tion.jda.api.JDABuilder;
-import net.dv8tion.jda.api.requests.GatewayIntent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
-import java.util.EnumSet;
-
 class MayuListener extends ListenerAdapter{
-  public enum Mode{Standard, Fibonacci, Even}
-  private Mode mode;
-  private Evaluator evaluator;
-  private long currentCount;
-  private long priorCount;
+  private DiscordDriver driver;
 
-  public MayuListener(Mode mode){
-    this.evaluator = new Evaluator();
-    this.mode = mode;
-
-    switch(mode){
-      case Standard:
-      case Fibonacci:
-        this.currentCount = 1;
-        this.priorCount = 1;
-        break;
-      case Even:
-        this.currentCount = 2;
-        this.priorCount = 2;
-        break;
-    }
+  public void SetDriver(DiscordDriver driver){
+    this.driver = driver;
   }
-
-  private long GetNumber(String expression){
-    var result = evaluator.Process(expression);
-    if(result.IsFloating()){
-      return (long)result.GetFloating();
-    }
-    else if(result.IsIntegral()){
-      return result.GetInt();
-    }
-    else{
-      throw new RuntimeException("Driver.GetNumber(): " +
-          "Failed to get a integral or floating from the expression");
-    }
-  }
-
-  private boolean NumberSuccess(long num){
-    switch(mode){
-      case Standard:
-        return StandardCheck(num);
-      case Fibonacci:
-        return FibonacciCheck(num);
-      case Even:
-        return EvenCheck(num);
-      default:
-        return false;
-    }
-  }
-
-  private boolean StandardCheck(long num){
-    if(num == currentCount +1){
-      currentCount = currentCount+1;
-      return true;
-    }
-    else{
-      ResetCount(1);
-      return false;
-    }
-  }
-
-  private boolean EvenCheck(long num){
-    if(num == currentCount + 2){
-      currentCount = currentCount + 2;
-      return true;
-    }
-    else{
-      ResetCount(2);
-      return false;
-    }
-  }
-
-  private boolean FibonacciCheck(long num){
-    if(num == currentCount){
-      long tmp = priorCount;
-      priorCount = currentCount;
-      currentCount += tmp;
-      return true;
-    }
-    else{
-      ResetCount(1);
-      RestPriorCount(1);
-      return false;
-    }
-  }
-
-  private void ResetCount(long initial){ currentCount = initial; }
-  private void RestPriorCount(long initial){ priorCount = initial;}
 
   @Override
   public void onMessageReceived(MessageReceivedEvent event){
@@ -114,8 +24,8 @@ class MayuListener extends ListenerAdapter{
       var message = event.getMessage().getContentRaw();
       // Evaluate the message using an evaluator
       try{
-        long num = GetNumber(message);
-        if(NumberSuccess(num)){
+        long num = driver.GetNumber(message);
+        if(driver.NumberSuccess(num)){
           channel.sendMessage(String.format("Nyanderful!")).queue(); 
         }
         else{
