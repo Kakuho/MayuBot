ea09b25155a03f421723e9b2a6646ca7ddc2f7b4
ka <kaku.h9@gmail.com>
2025-09-10 16:05:43 +0100

    drivers: refactored the cli driver

--- src/main/java/mayubot/drivers/CliDriver.java
+++ src/main/java/mayubot/drivers/CliDriver.java
@@ -8,30 +8,16 @@ import java.lang.RuntimeException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class CliDriver{
-  public enum Mode{Standard, Fibonacci, Even}
-  private Mode mode;
-  private Evaluator evaluator;
-  private long currentCount;
-  private long priorCount;
+public final class CliDriver extends Driver{
   private Scanner sc;
 
-  public CliDriver(Mode mode){
-    this.evaluator = new Evaluator();
-    this.sc = new Scanner(System.in);
-    this.mode = mode;
+  public CliDriver(){
+    super();
+  }
 
-    switch(mode){
-      case Standard:
-      case Fibonacci:
-        this.currentCount = 1;
-        this.priorCount = 1;
-        break;
-      case Even:
-        this.currentCount = 2;
-        this.priorCount = 2;
-        break;
-    }
+  public CliDriver(Mode mode){
+    super(mode);
+    sc = new Scanner(System.in);
   }
 
   private String GetUserInput(){
@@ -39,98 +25,37 @@ public class CliDriver{
     return inputstr;
   }
 
-  private long GetNumber(String expression){
-    var result = evaluator.Process(expression);
-    if(result.IsFloating()){
-      return (long)result.GetFloating();
-    }
-    else if(result.IsIntegral()){
-      return result.GetInt();
-    }
-    else{
-      throw new RuntimeException("Driver.GetNumber(): " +
-          "Failed to get a integral or floating from teh expression");
-    }
-  }
+  // main loop handlers
 
-  private boolean NumberSuccess(long num){
-    switch(mode){
-      case Standard:
-        return StandardCheck(num);
-      case Fibonacci:
-        return FibonacciCheck(num);
-      case Even:
-        return EvenCheck(num);
-      default:
-        return false;
-    }
-  }
-
-  private boolean StandardCheck(long num){
-    if(num == currentCount +1){
-      currentCount = currentCount+1;
-      return true;
-    }
-    else{
-      ResetCount(1);
-      return false;
-    }
-  }
-
-  private boolean EvenCheck(long num){
-    if(num == currentCount + 2){
-      currentCount = currentCount + 2;
-      return true;
-    }
-    else{
-      ResetCount(2);
-      return false;
-    }
-  }
-
-  private boolean FibonacciCheck(long num){
-    if(num == currentCount){
-      long tmp = priorCount;
-      priorCount = currentCount;
-      currentCount += tmp; 
-      return true;
-    }
-    else{
-      ResetCount(1);
-      RestPriorCount(1);
-      return false;
-    }
-  }
-
-  private void ResetCount(long initial){ currentCount = initial; }
-  private void RestPriorCount(long initial){ priorCount = initial;}
-
-  private void DoEnd(){
+  private void HandleEnd(){
     System.out.println("Ending the Counting Game!");
   }
 
-  private void DoFailure(){
+  @Override
+  protected void HandleFailure(){
     System.out.println("You miscounted! Count is resetting back to 1");
   }
 
-  private void DoSuccess(){
+  @Override
+  protected void HandleSuccess(){
     System.out.println("Success!");
   }
 
+  @Override
   public void Run(){
     while(true){
       var inputString = GetUserInput();
       if(inputString.equals("E")){
-        DoEnd();
+        HandleEnd();
         break;  
       }
       long num = GetNumber(inputString);
       System.out.println(String.format("Debug: Entered Number Parsed as %d", num));
       if(!NumberSuccess(num)){
-        DoFailure();
+        HandleFailure();
       }
       else{
-        DoSuccess();
+        HandleSuccess();
       }
     }
   }
